c319f40789a63d7034b8ae53e908b86ea7133cbf
shundroid <shundroidk@gmail.com>
2019-04-02 11:02:39 +0900

    detect minimum declarations

--- index.js
+++ index.js
@@ -1,10 +1,51 @@
 const acorn = require('acorn')
 const fs = require('fs')
 
-fs.readFile('./test-files/main.js', (err, data) => {
+const Program = require('./lib/program')
+const Declaration = require('./lib/declaration')
+
+const programs = []
+
+const entryFile = './test-files/main.js'
+
+fs.readFile(entryFile, (err, data) => {
   if (err) {
     console.error(err)
     return
   }
-  console.log(acorn.Parser.parse(data.toString()))
+  const program = readProgram(entryFile, acorn.Parser.parse(data.toString()))
 })
+
+function readProgram(filePath, program) {
+  const { declarations, references } = readFunction(['root'], program)
+  return new Program(filePath, declarations, references)
+}
+
+function readFunction(belongs, fn) {
+  const declarations = []
+  const references = []
+  for (let node of fn.body) {
+    switch (node.type) {
+      case 'VariableDeclaration':
+        for (let declarator of node.declarations) {
+          if (declarator.type === 'VariableDeclarator') {
+            declarations.push(new Declaration(declarator.id.name, belongs))
+          }
+        }
+        break
+      case 'FunctionDeclaration':
+        declarations.push(new Declaration(node.id.name, belongs))
+        const fn = readFunction([
+          ...belongs,
+          node.id.name
+        ], node.body)
+        declarations.push(...fn.declarations)
+        references.push(...fn.references)
+    }
+  }
+  return { declarations, references }
+}
+
+function readObject() {
+
+}
--- test-files/main.js
+++ test-files/main.js
@@ -1,7 +1,14 @@
 var a = 0;
 
+const b = 0, d = 2;
+
+let f = 10, g;
+
 function fn() {
   a++;
+  function fn2() {
+
+  }
 }
 
 function main() {
