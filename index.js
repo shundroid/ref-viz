3b2ca00afa27770475eb484f8e9e73eb385fcb41
shundroid <shundroidk@gmail.com>
2019-04-02 12:22:52 +0900

    supported obj

--- index.js
+++ index.js
@@ -14,6 +14,8 @@ fs.readFile(entryFile, (err, data) => {
     return
   }
   const program = readProgram(entryFile, acorn.Parser.parse(data.toString()))
+  console.log(program)
+  console.log(program.declarations.forEach(declaration => console.log(declaration.belongs)))
 })
 
 function readProgram(filePath, program) {
@@ -31,6 +33,27 @@ function readFunction(belongs, fn) {
           if (declarator.type === 'VariableDeclarator') {
             declarations.push(new Declaration(declarator.id.name, belongs))
           }
+          if (declarator.init) {
+            switch (declarator.init.type) {
+              case 'ObjectExpression':
+                const obj = readObject([
+                  ...belongs,
+                  declarator.id.name
+                ], declarator.init)
+                declarations.push(...obj.declarations)
+                references.push(...obj.references)
+                break
+              case 'ArrowFunctionExpression':
+              case 'FunctionExpression':
+                const fn = readFunction([
+                  ...belongs,
+                  declarator.id.name
+                ], declarator.init.body.body)
+                declarations.push(...fn.declarations)
+                references.push(...fn.references)
+                break
+            }
+          }
         }
         break
       case 'FunctionDeclaration':
@@ -46,6 +69,29 @@ function readFunction(belongs, fn) {
   return { declarations, references }
 }
 
-function readObject() {
-
+function readObject(belongs, objectExpression) {
+  const declarations = []
+  const references = []
+  for (let property of objectExpression.properties) {
+    declarations.push(new Declaration(property.key.name, belongs))
+    switch (property.value.type) {
+      case 'FunctionExpression':
+        const fn = readFunction([
+          ...belongs,
+          property.key.name
+        ], property.value.body)
+        declarations.push(...fn.declarations)
+        references.push(...fn.references)
+        break
+      case 'ObjectExpression':
+        const obj = readObject([
+          ...belongs,
+          property.key.name
+        ], property.value)
+        declarations.push(...obj.declarations)
+        references.push(...obj.references)
+        break
+    }
+  }
+  return { declarations, references }
 }
--- test-files/main.js
+++ test-files/main.js
@@ -4,6 +4,19 @@ const b = 0, d = 2;
 
 let f = 10, g;
 
+let hoge = {
+  aa: "aaa",
+  f: () => {
+    console.log('hoge')
+  },
+  f2() {
+    console.log("fuga")
+  },
+  obj: {
+    aa: "aaa"
+  }
+}
+
 function fn() {
   a++;
   function fn2() {
