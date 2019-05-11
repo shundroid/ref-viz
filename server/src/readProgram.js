c3d9e7065b594ae5b04604894eb65f868c2c7986
shundroid <shundroidk@gmail.com>
2019-05-11 14:48:46 +0900

    fixed client

--- server/src/readProgram.js
+++ server/src/readProgram.js
@@ -2,7 +2,7 @@ const Program = require('./lib/program')
 const readFunction = require('./readFunction')
 
 function readProgram(filePath, program) {
-  const result = readFunction([], program)
+  const result = readFunction(program)
   return new Program(filePath, result)
 }
 
